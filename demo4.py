c3dba3ec99b10efc008f599bb4591f2cebb1befc
santosh0634 <45452833+santosh0634@users.noreply.github.com>
2018-12-13 21:27:03 +0530

    simple addition prog

---
+++ demo4.py
@@ -0,0 +1,8 @@
+def display(empno=0,name="none",salary=0.0000):
+    print("EMP NO=",empno)
+    print("EMP NAME=",name)
+    print("EMP SALARY",salary)
+display(salary="santosh")
+display()
+display(salary=180000,empno=25,name="santosh")
+
