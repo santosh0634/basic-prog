46d246814123ae242d13495410203e0cf3d36590
santosh0634 <45452833+santosh0634@users.noreply.github.com>
2018-12-13 21:10:59 +0530

    example prog for fun

---
+++ 11.py
@@ -0,0 +1,14 @@
+#demo prog for funtion
+def all():
+    first_number=int(input("enter first number"))
+    second_number=int(input("enter second number"))
+    #here first number is assign to variable "a"
+    #here second number is assign to variable "b"
+    a=first_number
+    b=second_number
+    print(a+b)
+    print(a-b)
+    print(a*b)
+    print(a/b)
+    #function call
+all()
