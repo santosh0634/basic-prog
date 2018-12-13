7ab38d3ba00ca1808cac4692d9c21839232f6f12
santosh0634 <45452833+santosh0634@users.noreply.github.com>
2018-12-13 21:41:04 +0530

    static prog

---
+++ demo7.py
@@ -0,0 +1,13 @@
+def div(no1,no2):
+    print(mul(no1,no2))
+    return no1/no2
+def mul(no1,no2):
+    print(sub(no1,no2))
+    return no1*no2
+def sub(no1,no2):
+    print(add(no1,no2))
+    return no1-no2
+def add(no1,no2):
+    return no1+no2
+
+print(div(10,2))
