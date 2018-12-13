13fc4a05aef89440024248b99416c4fd1ea223b6
santosh0634 <45452833+santosh0634@users.noreply.github.com>
2018-12-13 22:00:48 +0530

    student report

---
+++ ghodt.py
@@ -0,0 +1,8 @@
+#students report
+no1=input("student no:")
+name=input("student name:")
+m1=int(input("sub1="))
+m2=int(input("sub2="))
+m3=int(input("sub3="))
+print("avg=",(m1+m2+m3)/3)
+print("total marks=",(m1+m2+m3))
