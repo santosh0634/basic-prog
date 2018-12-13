f06127520ef6c8dd7fa9fb57c4a7294d89a00eed
santosh0634 <45452833+santosh0634@users.noreply.github.com>
2018-12-13 21:35:00 +0530

    basic emp

---
+++ demo5.py
@@ -0,0 +1,7 @@
+def display(salary,name="none",idno=10):
+    print("EMP SALARY=",salary)
+    print("employee name=",name)
+    print("employee idno=",idno)
+
+display("santosh")
+display(500000,"santosh",15)
