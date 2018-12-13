add48d0327a9326723a34b7212b86645e677b0c0
santosh0634 <45452833+santosh0634@users.noreply.github.com>
2018-12-13 22:06:31 +0530

    employee salary

--- ghodt.py
+++ ghodt.py
@@ -1,8 +1,8 @@
-#students report
-no1=input("student no:")
-name=input("student name:")
-m1=int(input("sub1="))
-m2=int(input("sub2="))
-m3=int(input("sub3="))
-print("avg=",(m1+m2+m3)/3)
-print("total marks=",(m1+m2+m3))
+#emp salary
+id=input("enter emp id:")
+name=input("emp name:")
+sal=int(input("b.sal"))
+hra=int(input("enter hra"))
+da=int(input("enter da"))
+
+print("gross salary=",(sal+hra+da))
